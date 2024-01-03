e4b6ff423c02ec39ab55244c487e5106e3ca50d9
Kyle <kmthayer@uw.edu>
2024-01-03 11:19:54 -0800

    add demo of function definitions

--- lecture_01_simple_servers/demo_1_nodejs/app.js
+++ lecture_01_simple_servers/demo_1_nodejs/app.js
@@ -6,3 +6,17 @@ let hours = days * 24
 let minutes = hours * 60
 
 console.log("There are " + minutes + " minutes in a year")
+
+// note: these are three ways of writing functions that end up equivalent
+
+function example_function(a, b){
+    return a + b;
+}
+
+let example_function = function(a, b) {
+    return a + b;
+}
+
+let example_function = (a, b) => {
+    return a + b;
+}
