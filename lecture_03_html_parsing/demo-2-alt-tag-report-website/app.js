e601c70ca2800970a3e6b3bd72e85ac6b0112235
Kyle <kmthayer@uw.edu>
2024-01-10 11:27:54 -0800

    start demo 2 alt text report website

---
+++ lecture_03_html_parsing/demo-2-alt-tag-report-website/app.js
@@ -0,0 +1,43 @@
+import { promises as fs } from 'fs'
+import pluralize from 'pluralize'
+import express from 'express'
+const app = express()
+
+app.get("/", async (req, res) => {
+    console.log("request to '/', sending back html")
+    res.type('html')
+    let fileContents = await fs.readFile("index.html")
+    res.send(fileContents)
+})
+
+app.get("/style.css", async (req, res) => {
+    console.log("request to '/style.css', sending back css content")
+    res.type('css')
+    let fileContents = await fs.readFile("style.css")
+    res.send(fileContents)
+})
+
+app.get("/index.js", async (req, res) => {
+    console.log("request to '/index.js', sending back css content")
+    res.type('js')
+    let fileContents = await fs.readFile("index.js")
+    res.send(fileContents)
+})
+
+app.get("/favicon.ico", async (req, res) => {
+    console.log("request to '/favicon.ico', sending back css content")
+    res.type('bmp') // can also do a png file
+    let fileContents = await fs.readFile("favicon.ico")
+    res.send(fileContents)
+})
+
+app.get("/api/pluralize", (req, res) => {
+    let inputWord = req.query.word
+    let pluralWord = pluralize(inputWord)
+    res.type("txt")
+    res.send(pluralWord)
+})
+
+app.listen(3000, () => {
+    console.log("Example app listening at http://localhost:3000")
+})
