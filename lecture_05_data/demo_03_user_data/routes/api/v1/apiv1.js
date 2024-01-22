3026a935644ddfd39019799b22b53d75e23e45eb
Kyle <kmthayer@uw.edu>
2024-01-22 12:14:10 -0800

    post user now saves to file

--- lecture_05_data/demo_03_user_data/routes/api/v1/apiv1.js
+++ lecture_05_data/demo_03_user_data/routes/api/v1/apiv1.js
@@ -2,6 +2,16 @@ import {promises as fs} from 'fs'
 import express from 'express';
 var router = express.Router();
 
+router.post('/users', async (req, res) => {
+    console.log(req.body)
+
+    await fs.writeFile("data/users.json", JSON.stringify(req.body))
+
+    res.send("success")
+})
+
+
+// TODO: make this GET users
 router.get('/getPterosaurs', async (req, res) =>{
     // load data file
     const dataString  = await fs.readFile("data/pterosaur.json")
