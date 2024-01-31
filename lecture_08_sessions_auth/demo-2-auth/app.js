a9c561c8e7e8e7cd930a607d49399ce5d2f8e09a
Kyle <kmthayer@uw.edu>
2024-01-31 11:39:20 -0800

    start auth demo

---
+++ lecture_08_sessions_auth/demo-2-auth/app.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import sessions from 'express-session'
+
+import usersRouter from './routes/users.js';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const oneDay = 1000 * 60 * 60 * 24
+app.use(sessions({
+    secret: "this is some secret key I am making up v45v;lkjgdsal;nwqt49asglkn",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}))
+
+app.use((req, res, next) =>{
+    console.log("session info:", req.session)
+    next();
+})
+
+app.use('/users', usersRouter);
+
+export default app;
