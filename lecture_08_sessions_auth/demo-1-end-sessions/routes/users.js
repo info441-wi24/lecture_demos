43ac8a5937aefe0758e98014c9060849ef6f0029
Kyle <kmthayer@uw.edu>
2024-01-31 12:13:52 -0800

    finish auth demo

--- lecture_08_sessions_auth/demo-1-end-sessions/routes/users.js
+++ lecture_08_sessions_auth/demo-1-end-sessions/routes/users.js
@@ -3,33 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  if(req.session.userid){
-    res.send('Here is the information for you (' + req.session.userid + ")");
+  if(req.session.isAuthenticated){
+    res.send(`
+    responding with information about user
+    with the name: ${req.session.account.name}
+    and the username: ${req.session.account.username}
+    `);
   }else{
     res.send("Error: you must be logged in");
   }
 });
 
-router.post("/login", function(req, res, next) {
-  //check username and password
-  if(req.body.username == "kylethayer" && req.body.password == "asdasd"){
-    //mark session as logged in
-    req.session.userid = "kylethayer"
-    res.send("you logged in")
-  } else if(req.body.username == "anotheruser" && req.body.password == "pwd"){
-    //mark session as logged in
-    req.session.userid = "anotheruser"
-    res.send("you logged in")
-  } else{
-    //don't update logged in user
-    res.send("wrong login info")
-  }  
-})
-
-router.post("/logout", function(req, res, next) {
-  req.session.destroy()
-  res.send("you are logged out")
-})
-  
 
 export default router;
