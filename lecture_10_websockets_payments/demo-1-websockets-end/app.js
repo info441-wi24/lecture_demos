02c285c12d635a3328f56d0e63a03d374e782ece
Kyle <kmthayer@uw.edu>
2024-02-12 10:59:25 -0800

    establish websocket connections

--- lecture_10_websockets_payments/demo-1-websockets-end/app.js
+++ lecture_10_websockets_payments/demo-1-websockets-end/app.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import enableWs from 'express-ws'
 
 const app = express()
+enableWs(app)
+
+app.ws('/chatSocket', (ws, res) => {
+    console.log("the browser established a ws connection")
+})
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + "/index.html")
