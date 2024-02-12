0f8f458088619444f22111718dea85283f860306
Kyle <kmthayer@uw.edu>
2024-02-12 11:49:59 -0800

    add model code for items

--- lecture_10_websockets_payments/demo-2-store-end/models.js
+++ lecture_10_websockets_payments/demo-2-store-end/models.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-connectDB();
 
 let models = {};
 
-async function connectDB() {
-  console.log("connecting to mongodb");
-  // Put your MongoDB Atlas connection string in, or
-  await mongoose.connect('mongodb://localhost:27017/store');
-  console.log("connected to mongodb");
+console.log("connecting to mongodb");
+// Put your MongoDB Atlas connection string in, or
+await mongoose.connect('mongodb://localhost:27017/store');
+console.log("connected to mongodb");
 
-  //Add schemas and models
+//Add schemas and models
 
-  
-  console.log("finished creating models");
-}
+const itemSchema = new mongoose.Schema({
+  name: String,
+  price: Number
+})
+
+models.Item = mongoose.model("Item", itemSchema)
+
+console.log("finished creating models");
 
 export default models;
