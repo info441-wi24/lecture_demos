a796063cb3d3c028493f0e43e8f95a25553eae7c
Kyle <kmthayer@uw.edu>
2024-02-12 11:13:14 -0800

    send message back across all websockets

--- lecture_10_websockets_payments/demo-1-websockets-end/app.js
+++ lecture_10_websockets_payments/demo-1-websockets-end/app.js
@@ -4,11 +4,24 @@ import enableWs from 'express-ws'
 const app = express()
 enableWs(app)
 
+// track websocket connections
+let allSockets = []
+
 app.ws('/chatSocket', (ws, res) => {
     console.log("the browser established a ws connection")
 
+    // add this ws to the global array tracking all websockets
+    allSockets.push(ws)
+
     ws.on('message', msg => {
         console.log("msg: " + msg)
+        // TODO: I want to send the message I got
+        // to all the other browsers connected
+        // (using their websockets)
+        allSockets.forEach(socket => {
+            socket.send(msg)
+        })
+
     })
 })
 
