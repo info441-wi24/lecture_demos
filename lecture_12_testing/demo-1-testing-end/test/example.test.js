7e68d43b534c4f3fd0936ecd18f3d6fbda75ac03
Kyle <kmthayer@uw.edu>
2024-02-14 11:14:37 -0800

    more demo tests

--- lecture_12_testing/demo-1-testing-end/test/example.test.js
+++ lecture_12_testing/demo-1-testing-end/test/example.test.js
@@ -4,4 +4,12 @@ describe('DemoTest', function(){
     it('should say 2+2=4', function(){
         assert.equal(2+2, 4)
     })
+
+    it('should say 4+4 = 8', function(){
+        assert.equal(4+4, 8)
+    })
+
+    // it('should say 1+1 = 3', function(){
+    //     assert.equal(1+1, 3)
+    // })
 })
